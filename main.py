13f34ad7b5a68005d7901578412969a89ced1cb0
det048 <shishir.sanjeeva@dheyatech.com>
2026-09-23 19:31:32 +0530

    Update modal analysis parameters and speed range; adjust mode number and UCS map settings

--- main.py
+++ main.py
@@ -27,24 +27,25 @@ rotor.plot_rotor(check_sld=True).show()  # Plot the rotor model
 # static.plot_free_body_diagram().show()  # Plot the free body diagram of the rotor
 
 # ***********Modal ANALYSIS****************
-rotor_speed = 110000/60 * 2 * np.pi # rad/s
-modal = rotor.run_modal(rotor_speed, num_modes=15)
-# print(f"Undamped natural frequencies:\n {modal.wn}")
+rotor_speed = 90000/60 * 2 * np.pi # rad/s
+
+modal = rotor.run_modal(rotor_speed, num_modes=20)
+print(f"Undamped natural frequencies:\n {modal.wn}")
 
 #enter the mode number you want to plot
-mode = 6
+mode = 9
 for i in range(mode):
     # modal.plot_mode_2d(mode, frequency_units="Hz").show()  # Plot the 2D mode shape of the rotor
     modal.plot_mode_3d(i, frequency_units="RPM").show()  # Plot the 3D mode shape of the rotor
 
 # *****************Campbell****************
 samples = 31
-speed_range = np.linspace(1, rotor_speed, samples)
+speed_range = np.linspace(1, rotor_speed *1.3, samples)
 
 campbell = rotor.run_campbell(speed_range)
 campbell.plot([1, 2],frequency_units="Hz").show()
 
 # ************************* UCS Map*************************
 stiff_range = (4, 9)
-ucs_results = rotor.run_ucs(stiffness_range=stiff_range, num=20, num_modes=25,)
+ucs_results = rotor.run_ucs(stiffness_range=stiff_range, num=20, num_modes=25, kxx=0.1e6, kyy=10e6)
 ucs_results.plot(frequency_units="Hz").show()  # Plot the UCS map
