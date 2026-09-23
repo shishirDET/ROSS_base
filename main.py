bdb2cafe5980e4e24d9bce548f24a5d2e74b67bf
det048 <shishir.sanjeeva@dheyatech.com>
2026-09-23 17:01:18 +0530

    Update rotor model path and streamline analysis code; replaced rotor1 with rotor

--- main.py
+++ main.py
@@ -8,20 +8,42 @@ import plotly.io as pio
 pio.renderers.default = "browser"
 
 # ======================================================================================
-path = "rotorModels/testRotor3.toml"
+path = "rotorModels/kj450.toml"
 
 if not Path(path).exists():
     print(f"File {path} does not exist. Please create the rotor model first.")
     exit()
 else:
-    rotor1 = rs.Rotor.load(path)
+    rotor = rs.Rotor.load(path)
     print(f"Rotor model loaded from {path}")
-print("Rotor total mass = ", np.round(rotor1.m, 2))
-print("Rotor center of gravity =", np.round(rotor1.CG, 2))
+print("Rotor total mass = ", np.round(rotor.m, 2))
+print("Rotor center of gravity =", np.round(rotor.CG, 2))
 
 # plotting the rotor model
-rotor1.plot_rotor(check_sld=True).show()  # Plot the rotor model
+# rotor.plot_rotor(check_sld=True).show()  # Plot the rotor model
 
 # ***********STATIC ANALYSIS****************
-static = rotor1.run_static()
-static.plot_free_body_diagram().show()  # Plot the free body diagram of the rotor
+# static = rotor.run_static()
+# static.plot_free_body_diagram().show()  # Plot the free body diagram of the rotor
+
+# ***********Modal ANALYSIS****************
+rotor_speed = 110000/60 * 2 * np.pi # rad/s
+modal = rotor.run_modal(rotor_speed, num_modes=15)
+print(f"Undamped natural frequencies:\n {modal.wn}")
+
+#enter the mode number you want to plot
+mode = 3
+# modal.plot_mode_2d(mode, frequency_units="Hz").show()  # Plot the 2D mode shape of the rotor
+modal.plot_mode_3d(mode, frequency_units="Hz").show()  # Plot the 3D mode shape of the rotor
+
+# *****************Campbell****************
+samples = 31
+speed_range = np.linspace(1, rotor_speed, samples)
+
+campbell = rotor.run_campbell(speed_range)
+campbell.plot([1, 2],frequency_units="Hz").show()
+
+# Running and plotting UCS Map
+stiff_range = (4, 9)
+ucs_results = rotor.run_ucs(stiffness_range=stiff_range, num=20, num_modes=16)
+ucs_results.plot(frequency_units="Hz").show()  # Plot the UCS map
