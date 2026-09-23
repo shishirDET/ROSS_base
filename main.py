0a40154c43b2e0bcd43ad6418615b3ef5fbc09d1
det048 <shishir.sanjeeva@dheyatech.com>
2026-09-23 18:40:20 +0530

    Update rotor plotting and analysis using ross3; enable rotor model visualization and adjust modal analysis parameters

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ print("Rotor total mass = ", np.round(rotor.m, 2))
 print("Rotor center of gravity =", np.round(rotor.CG, 2))
 
 # plotting the rotor model
-# rotor.plot_rotor(check_sld=True).show()  # Plot the rotor model
+rotor.plot_rotor(check_sld=True).show()  # Plot the rotor model
 
 # ***********STATIC ANALYSIS****************
 # static = rotor.run_static()
@@ -29,12 +29,13 @@ print("Rotor center of gravity =", np.round(rotor.CG, 2))
 # ***********Modal ANALYSIS****************
 rotor_speed = 110000/60 * 2 * np.pi # rad/s
 modal = rotor.run_modal(rotor_speed, num_modes=15)
-print(f"Undamped natural frequencies:\n {modal.wn}")
+# print(f"Undamped natural frequencies:\n {modal.wn}")
 
 #enter the mode number you want to plot
-mode = 3
-# modal.plot_mode_2d(mode, frequency_units="Hz").show()  # Plot the 2D mode shape of the rotor
-modal.plot_mode_3d(mode, frequency_units="Hz").show()  # Plot the 3D mode shape of the rotor
+mode = 6
+for i in range(mode):
+    # modal.plot_mode_2d(mode, frequency_units="Hz").show()  # Plot the 2D mode shape of the rotor
+    modal.plot_mode_3d(i, frequency_units="RPM").show()  # Plot the 3D mode shape of the rotor
 
 # *****************Campbell****************
 samples = 31
@@ -43,7 +44,7 @@ speed_range = np.linspace(1, rotor_speed, samples)
 campbell = rotor.run_campbell(speed_range)
 campbell.plot([1, 2],frequency_units="Hz").show()
 
-# Running and plotting UCS Map
+# ************************* UCS Map*************************
 stiff_range = (4, 9)
-ucs_results = rotor.run_ucs(stiffness_range=stiff_range, num=20, num_modes=16)
+ucs_results = rotor.run_ucs(stiffness_range=stiff_range, num=20, num_modes=25,)
 ucs_results.plot(frequency_units="Hz").show()  # Plot the UCS map
